6f11c5ac66fb81153797712d68278cfea24fda72
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-07-03 10:54:34 -0300

    putting my photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      hello world!
+      <Image
+        src="https://github.com/DevSamuelBrito.png" // Caminho para a imagem
+        alt="Descrição da Imagem"
+        width={500} // Largura da imagem
+        height={300} // Altura da imagem
+      />
     </main>
   );
 }
