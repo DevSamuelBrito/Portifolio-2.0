5d9585a6977340e1aa008686c3ffa73be134610e
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-07-08 23:15:36 -0300

    changing the language button image

--- app/_components/Home.tsx
+++ app/_components/Home.tsx
@@ -2,12 +2,7 @@ import Image from "next/image";
 const Home = () => {
   return (
     <main className="flex flex-col items-center justify-center p-24">
-      <Image
-        src="/BR.png" // Caminho da imagem relativo à pasta `public`
-        alt="Brasil"
-        width={32} // Largura da imagem
-        height={32} // Altura da imagem
-      />
+      <p>Olá mundo</p>
     </main>
   );
 };
--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -1,14 +1,22 @@
 //fazendo os imports necessários
-'use client';
+"use client";
 import { useState } from "react";
 import Link from "next/link";
-import { FaBars, FaTimes } from "react-icons/fa"; 
-
+import Image from "next/image";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   //criando um estado para controlar o menu
   const [isOpen, setIsOpen] = useState(false);
- //função para abrir e fechar o menu
+  const [language, setLanguage] = useState("/BR.png");
+  const urlImageBrasil = "/BR.png";
+  const urlImageEua = "/ES.png";
+
+  const changeImageButton = () => {
+    setLanguage(language != urlImageBrasil ? urlImageBrasil : urlImageEua);
+    console.log("changed language");
+  };
+  //função para abrir e fechar o menu
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
@@ -20,27 +28,53 @@ const Navbar = () => {
           <p className="text-white text-2xl font-bold">Portifolio</p>
         </div>
         <div className="hidden md:flex space-x-14">
-          <Link href="/" className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out">
+          <Link
+            href="/"
+            className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out"
+          >
             Home
           </Link>
-          <Link href="/" className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out">
+          <Link
+            href="/"
+            className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out"
+          >
             About
           </Link>
-          <Link href="/" className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out">
+          <Link
+            href="/"
+            className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out"
+          >
             Projects
           </Link>
         </div>
         <div className="hidden md:flex items-center space-x-6 lg:mr-10">
-          <Link href="https://www.linkedin.com/in/samuel-fava-de-brito/" target="_blank">
+          <Link
+            href="https://www.linkedin.com/in/samuel-fava-de-brito/"
+            target="_blank"
+          >
             <button className="text-white bg-blue-500 font-bold rounded-full px-4 py-2 hover:scale-125 transition duration-300 ease-in-out hover:shadow-custom-color">
               Contact
             </button>
           </Link>
-          <button className="text-white pl-3">Language</button>
+          <Image
+            onClick={changeImageButton}
+            src={language}
+            alt="change language"
+            width={32}
+            height={32}
+            className="cursor-pointer"
+          ></Image>
         </div>
         <div className="md:hidden flex items-center">
-          <button onClick={toggleMenu} className="text-white focus:outline-none">
-            {isOpen ? <FaTimes className="h-6 w-6" /> : <FaBars className="h-6 w-6" />}
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none"
+          >
+            {isOpen ? (
+              <FaTimes className="h-6 w-6" />
+            ) : (
+              <FaBars className="h-6 w-6" />
+            )}
           </button>
         </div>
       </div>
@@ -58,14 +92,24 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link href="https://www.linkedin.com/in/samuel-fava-de-brito/" target="_blank">
+              <Link
+                href="https://www.linkedin.com/in/samuel-fava-de-brito/"
+                target="_blank"
+              >
                 <button className="text-white bg-red-500 font-bold rounded-full px-4 py-2">
                   Contact
                 </button>
               </Link>
             </li>
             <li>
-              <button className="text-white">Language</button>
+              <Image
+                onClick={changeImageButton}
+                src={language}
+                alt="change language"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              ></Image>{" "}
             </li>
           </ul>
         </div>
