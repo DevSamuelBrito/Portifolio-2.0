a3523d8158feea1b0979388e79810cfe19800f32
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-08-06 15:50:11 -0300

    refazendo a Navbar para computadores

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -22,98 +22,43 @@ const Navbar = () => {
   };
   //retornando toda a rederização da NavBar
   return (
-    <header>
-      <div className=" relative z-10 mx-auto px-4 flex items-center justify-between h-14">
-        <div className="flex items-center ml-0 md:ml-20">
-          <p className="text-white text-2xl font-bold">Portifolio</p>
-        </div>
-        <div className="hidden md:flex space-x-14">
+    <header className="bg-black w-full h-16 flex justify-center items-center">
+      <section className=" px-8 w-full max-w-screen-lg flex justify-between">
+        <nav className="flex items-center justify-center">
+          <Link href="/">
+            <h1 className="text-white text-xl font-bold">Portifolio</h1>
+          </Link>
+        </nav>
+        <div className="flex items-center justify-between w-96">
           <Link
+            className="text-white hover:text-gray-400 hover:scale-110 transition duration-300 ease-in-out"
             href="/"
-            className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out"
           >
             Home
           </Link>
           <Link
+            className="text-white hover:text-gray-400 hover:scale-110 transition duration-300 ease-in-out"
             href="/"
-            className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out"
           >
             About
           </Link>
           <Link
+            className="text-white hover:text-gray-400 hover:scale-110 transition duration-300 ease-in-out"
             href="/"
-            className="text-white hover:text-blue-400 hover:scale-110 transition duration-300 ease-in-out"
           >
             Projects
           </Link>
-        </div>
-        <div className="hidden md:flex items-center space-x-6 lg:mr-10">
-          <Link
-            href="https://www.linkedin.com/in/samuel-fava-de-brito/"
-            target="_blank"
-          >
-            <button className="text-white bg-blue-500 font-bold rounded-full px-4 py-2 hover:scale-125 transition duration-300 ease-in-out hover:shadow-custom-color">
-              Contact
-            </button>
-          </Link>
-          <Image
-            onClick={changeImageButton}
-            src={language}
-            alt="change language"
-            width={32}
-            height={32}
-            className="cursor-pointer"
-          ></Image>
-        </div>
-        <div className="md:hidden flex items-center">
+
           <button
-            onClick={toggleMenu}
-            className="text-white focus:outline-none"
+            onClick={() => {
+              console.log("Console");
+            }}
+            className="border text-white cursor-pointer rounded-2xl bg-transparent px-6 py-1 hover:bg-white hover:text-black hover:font-bold hover:scale-110 transition duration-300 ease-in-out"
           >
-            {isOpen ? (
-              <FaTimes className="h-6 w-6" />
-            ) : (
-              <FaBars className="h-6 w-6" />
-            )}
+            Contact
           </button>
         </div>
-      </div>
-      {isOpen && (
-        <div className="md:hidden absolute top-14 left-0 w-full z-20 bg-red-100">
-          <ul className="flex flex-col items-center space-y-4 mt-4">
-            <li>
-              <Link href="/" className="text-white hover:text-blue-400">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="text-white hover:text-blue-400">
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://www.linkedin.com/in/samuel-fava-de-brito/"
-                target="_blank"
-              >
-                <button className="text-white bg-red-500 font-bold rounded-full px-4 py-2">
-                  Contact
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Image
-                onClick={changeImageButton}
-                src={language}
-                alt="change language"
-                width={32}
-                height={32}
-                className="cursor-pointer"
-              ></Image>{" "}
-            </li>
-          </ul>
-        </div>
-      )}
+      </section>
     </header>
   );
 };
