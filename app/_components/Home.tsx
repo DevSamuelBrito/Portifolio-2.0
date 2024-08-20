987fd581c4284b80e708a966163362df14cf239b
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-08-20 17:49:46 -0300

    ocultando a scrollbar

--- app/_components/Home.tsx
+++ app/_components/Home.tsx
@@ -1,23 +1,28 @@
 import Image from "next/image";
+import NavBar from "./NavBar";
+
 const Home = () => {
   return (
-    <main className=" snap-y snap-mandatory overflow-y-scroll">
-      <section className="h-screen snap-start flex relative items-center justify-center text-white">
-        <h1>Page 1</h1>
-      </section>
-      <section className="h-screen snap-start flex relative items-center justify-center text-white">
-        <h1>Page 2</h1>
-      </section>
-      <section className="h-screen snap-start flex relative items-center justify-center text-white">
-        <h1>Page 3</h1>
-      </section>
-      <section className="h-screen snap-start flex relative items-center justify-center text-white">
-        <h1>Page 4</h1>
-      </section>
-      <section className="h-screen snap-start flex relative items-center justify-center text-white">
-        <h1>Page 5</h1>
-      </section>{" "}
-    </main>
+    <>
+      <NavBar />
+      <main className=" snap-y snap-mandatory overflow-y-scroll">
+        <section className="h-screen snap-start flex relative items-center justify-center text-white">
+          <h1>Page 1</h1>
+        </section>
+        <section className="h-screen snap-start flex relative items-center justify-center text-white">
+          <h1>Page 2</h1>
+        </section>
+        <section className="h-screen snap-start flex relative items-center justify-center text-white">
+          <h1>Page 3</h1>
+        </section>
+        <section className="h-screen snap-start flex relative items-center justify-center text-white">
+          <h1>Page 4</h1>
+        </section>
+        <section className="h-screen snap-start flex relative items-center justify-center text-white">
+          <h1>Page 5</h1>
+        </section>{" "}
+      </main>
+    </>
   );
 };
 
