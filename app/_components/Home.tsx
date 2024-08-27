5b3e78c7e5a688c093d4fd5ece3ef18754807383
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-08-27 14:26:39 -0300

    arrumando o texto

--- app/_components/Home.tsx
+++ app/_components/Home.tsx
@@ -7,12 +7,12 @@ const Home = () => {
       <NavBar />
       <main className="snap-y snap-mandatory overflow-y-scroll scroll-smooth">
         <section className="h-screen snap-start flex items-center justify-center">
-          <div>
-            <div className="flex items-center space-x-48">
-              <h1 className="max-w-md break-words leading-relaxed text-white">
-              <h1 className="text-5xl font-semibold text-white">
+          <div className="flex items-center space-x-44">
+            <div className="max-w-md break-words leading-relaxed text-white">
+              <h1 className="text-5xl font-semibold">
                 Hi There I'm Samuel Fava de Brito
-              </h1 >
+              </h1>
+              <p>
                 Hello, welcome to my website! I am currently studying Analysis
                 and Systems Development at Fatec Rio Preto. I am preparing to
                 become a fullstack developer. I am currently looking for job
@@ -20,18 +20,20 @@ const Home = () => {
                 evolve in the area. If you are interested, contact me on one of
                 my social networks, and be sure to check out my GITHUB to see my
                 projects.
-              </h1>
-              <div>
-                <Image
-                  src="/photo.png"
-                  width={300}
-                  height={300}
-                  alt="Picture of the author"
-                />
-              </div>
+              </p>
+            </div>
+
+            <div className="ml-10">
+              <Image
+                src="/photo.png"
+                width={300}
+                height={300}
+                alt="Picture of the author"
+              />
             </div>
           </div>
         </section>
+
         <section className="h-screen snap-start flex relative items-center justify-center text-white">
           <h1>Page 2</h1>
         </section>
